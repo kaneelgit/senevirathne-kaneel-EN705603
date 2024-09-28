0c8d5fc763a72dcc1991a6207abc86e7865ca1bd
kaneelgit <kaneel.senevirathne@gmail.com>
2024-09-27 22:40:43 -0400

    some changes

--- techtrack/modules/inference/object_detection.py
+++ techtrack/modules/inference/object_detection.py
@@ -5,12 +5,13 @@ An object_detection module that contains a class called Model.
 """
 import os
 import cv2
-import numpy 
+import numpy as np
 
 class Model:
 
-    def __init__(self, model_weights_path: str = '../storage/models/yolo_model_1/yolov4-tiny-logistics_size_416_1.weights',/
-                 model_config_path: str = '../storage/models/yolo_model_1/yolov4-tiny-logistics_size_416_1.cfg'):
+    def __init__(self, model_weights_path: str = '../../storage/models/yolo_model_1/yolov4-tiny-logistics_size_416_1.weights',
+                 model_config_path: str = '../../storage/models/yolo_model_1/yolov4-tiny-logistics_size_416_1.cfg',
+                 names_path: str = '../../storage/models/yolo_model_1/logistics.names'):
         """
         Initialize the model given the version
         """ 
@@ -19,16 +20,32 @@ class Model:
         layer_names = self.net.getLayerNames()
         self.output_layers = [layer_names[i - 1] for i in self.net.getUnconnectedOutLayers()]
 
+        with open(names_path, "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+
     def predcit(self, preprocessed_frame):
 
         self.net.setInput(preprocessed_frame)
         outputs = self.net.forward(self.output_layers)
-        return
+        return outputs
 
     def post_process(self, predict_output, score_threshold = 0.5):
+        
+        #get bounding boxes, categories and 
+        bounding_boxes = []
+        class_categories = []
+        scores = []
 
         for feature_map in predict_output:
             for detection in feature_map:
                 boxes = detection[:4]
                 score = detection[4]
                 class_score = detection[5:]
+
+                #append data if greater than threshold
+                if score > score_threshold:
+                    bounding_boxes.append(boxes)
+                    class_categories.append(np.argmax(class_score))
+                    scores.append(score)
+
+        return bounding_boxes, class_categories, scores
