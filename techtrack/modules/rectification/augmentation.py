599af4691062ece630d9296d5bc3a2e1bcaddcab
kaneelgit <kaneel.senevirathne@gmail.com>
2024-10-03 23:05:59 -0400

    aug

--- techtrack/modules/rectification/augmentation.py
+++ techtrack/modules/rectification/augmentation.py
@@ -19,23 +19,16 @@ def resize_image(image, size=(416, 416)):
     resized_image = cv2.resize(image, size)
     return resized_image
 
-def rotate_image(image, angle=45):
-    (h, w) = image.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated_image = cv2.warpAffine(image, M, (w, h))
-    return rotated_image
+def vertical_flip(image):
+    flipped_image = cv2.flip(image, 0) 
+    return flipped_image
 
-def adjust_brightness(image, brightness_factor=1.2):
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    v = cv2.convertScaleAbs(v, alpha=brightness_factor)
-    final_hsv = cv2.merge((h, s, v))
-    bright_image = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+def convert_to_grayscale(image):
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
+    return grayscale_image
+
+
+def adjust_brightness(image, brightness_range=(0.5, 1.5)):
+    factor = np.random.uniform(brightness_range[0], brightness_range[1])
+    bright_image = cv2.convertScaleAbs(image, alpha=factor, beta=0)  
     return bright_image
-
-def translate_image(image, shift_x=50, shift_y=30):
-    (h, w) = image.shape[:2]
-    M = np.float32([[1, 0, shift_x], [0, 1, shift_y]])
-    shifted_image = cv2.warpAffine(image, M, (w, h))
-    return shifted_image
